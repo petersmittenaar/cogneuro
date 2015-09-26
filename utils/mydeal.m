0ad26277d04571c97298a63d54e17772181a87f7
Peter Smittenaar <peter@petersmittenaar.com>
2015-09-27 00:33:17 +0100

    typo in attribution to quentin

--- utils/mydeal.m
+++ utils/mydeal.m
@@ -1,6 +1,6 @@
 function mydeal(S);
 % Hands variables to workspace
-% by quentin guys, copied from his website.
+% by quentin huys, copied from his website.
 A=fieldnames(S);
 for k=1:length(A)
 	assignin('caller',A{k},S.(A{k}));
